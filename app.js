5dd776c3b581b14e8bf9a8f5ed1674848e43f411
KASHI TAMANG <tamang.kashi@gmail.com>
2023-01-23 12:22:37 -0800

    adding displayDogs() and able to render dog name to page

--- app.js
+++ app.js
@@ -6,11 +6,22 @@ const dogListContainer = document.getElementById('dog-list-container');
 let dogs = [];
 
 // on load
-
+// fetch all dogs
 window.addEventListener('load', async () => {
     const response = await getDogs();
     dogs = response;
     console.log(response);
+    displayDogs();
 });
-// fetch all dogs
+
 // render and append all dog cards to the container
+function displayDogs() {
+    dogListContainer.textContent = '';
+
+    dogs.forEach((dog) => {
+        const dogEl = renderDogCard(dog);
+        dogListContainer.append(dogEl);
+    });
+}
+
+displayDogs();
