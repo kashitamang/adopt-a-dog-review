35e986489d453be22e67217d5c0bb82784b6d686
KASHI TAMANG <tamang.kashi@gmail.com>
2023-01-23 12:15:15 -0800

    destructuring response in fetch, and able to log dogs array to the console.

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ import { renderDogCard } from './render-utils.js';
 
 const dogListContainer = document.getElementById('dog-list-container');
 
+let dogs = [];
+
 // on load
+
+window.addEventListener('load', async () => {
+    const response = await getDogs();
+    dogs = response;
+    console.log(response);
+});
 // fetch all dogs
 // render and append all dog cards to the container
